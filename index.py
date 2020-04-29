1a1d9a1b04c2df761f556c3d5656e396a44ed9c5
jrmylee <jrmylee@gmail.com>
2020-04-29 01:57:27 -0700

    CUTE DEgeNERATE GRAPH

--- index.py
+++ index.py
@@ -1,13 +1,15 @@
 from models.vertex import Vertex
 from process import *
 
-# number, 
+# number,
 # vertex a, vertex b, weight
 
-#parse function
+# parse function
+
+
 def parse(filename):
     file = open(filename, 'r')
-    contents_string = file.read() #reads in as a string
+    contents_string = file.read()  # reads in as a string
     contents = contents_string.split()
     # print(len(contents))
     contents = [float(elem) for elem in contents]
@@ -15,7 +17,7 @@ def parse(filename):
     n = contents[0]
     adj_list = [[0 for i in range(int(n))] for i in range(int(n))]
     i = 1
-    vertices = [None for i in range(int(n))]
+    vertices = [Vertex(i, 0, 0) for i in range(int(n))]
     while i < len(contents):
         v1 = int(contents[i])
         v2 = int(contents[i+1])
@@ -26,19 +28,37 @@ def parse(filename):
             vertices[v1] = vertex
         else:
             vertices[v1].set_degree(vertices[v1].degree+1)
-            vertices[v1].set_total_weight(vertices[v1].total_weight + contents[i+2])
+            vertices[v1].set_total_weight(
+                vertices[v1].total_weight + contents[i+2])
         if vertices[v2] == None:
             vertex = Vertex(v2, contents[i+2], 1)
             vertices[v2] = vertex
         else:
             vertices[v2].set_degree(vertices[v2].degree+1)
-            vertices[v2].set_total_weight(vertices[v2].total_weight+contents[i+2])
-        i+=3
+            vertices[v2].set_total_weight(
+                vertices[v2].total_weight+contents[i+2])
+        i += 3
     return adj_list, vertices
 
+
+def final_submission():
+    files = {
+        'small': 303,
+        'medium': 303,
+        'large': 400
+    }
+    for size in files:
+        for i in range(files[size]):
+            prefix = size + '-' + str(i+1)
+            filename = './inputs/' + prefix + '.in'
+            outputname = './outputs/' + prefix + '.out'
+            print("processing " + filename)
+            adj_list, vertices_list = parse(filename)
+            process(vertices_list, adj_list, outputname)
+
+
 def main():
-    adj_list, vertices_list = parse('./inputs/100.in')
-    process(vertices_list, adj_list)
-    for elem in vertices_list:
-        print(elem.total_weight)
+    final_submission()
+
+
 main()
--- models/T.py
+++ models/T.py
@@ -1,22 +1,35 @@
 from models.vertex import Vertex
 
+
 class T:
     def __init__(self, adj_list):
         # yer_mums_dahg is an adjacency list
         self.adj_list = adj_list
         self.vertices = set()
         self.neighbors_and_vertices = set()
+        self.edge_set = set()
 
-    #need to pass in vertices so that i can add the neighbor vertex
-    def addVertex(self, vertex, g_vertices):
+    # need to pass in vertices so that i can add the neighbor vertex
+    def addVertex(self, vertex, g_vertices, source_val, edge_weight):
         self.vertices.add(vertex)
         self.neighbors_and_vertices.add(vertex)
-        for i in len(self.adj_list[0]):
+        if source_val != -1:
+            self.edge_set.add((source_val, vertex.val, edge_weight))
+        for i in range(len(self.adj_list[0])):
             if self.adj_list[vertex.val][i] != 0:
                 self.neighbors_and_vertices.add(g_vertices[i])
 
     def isComplete(self, g_vertices):
         for vertex in g_vertices:
-            if vertex not in self.vertices:
+            if vertex not in self.neighbors_and_vertices:
                 return False
         return True
+
+    def printT(self, filename):
+        f = open(filename, 'w')
+
+        for vertex in self.vertices:
+            f.write(str(vertex.val) + " ")
+        f.write("\n")
+        for edge in self.edge_set:
+            f.write(str(edge[0]) + " " + str(edge[1]) + "\n")
